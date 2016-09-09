e46a265df4cfea01b81dc9c2b43faeb6244c2e4b
Berto Yáñez <berto@ber.to>
2016-09-09 10:54:44 +0200

    Multiple changes

--- app/public/js/controllers/panels/timeline.js
+++ app/public/js/controllers/panels/timeline.js
@@ -1,29 +1,18 @@
 'use strict';
 
-const Draggabilly = require('draggabilly');
-const _ = require('lodash');
-let scrubber;
 
 module.exports = ($panel) => {
-    const $scrubber = $panel.find('.scrubber');
-    const $slider = $panel.find('.slider');
-    const $canvas = $panel.find('.canvas');
+    const $handler = $panel.find('.handler');
+
     return {
         init: () => {
-            scrubber = new Draggabilly($scrubber.get(0), {
-                axis: 'y',
-                containment: $slider.get(0)
-            });
+
         },
         on: () => {
-
-            scrubber.on('dragMove', _.debounce((event, pointer) => {
-                const relativePosition = scrubber.position.y / $slider.height();
-
-                $canvas.css('transform', `translateY(${-1 * ($canvas.outerHeight() * relativePosition)}px)`);
-            }, 10));
+            $handler.css('position', 'fixed');
         },
         after: () => {
+            $handler.css('position', 'absolute');
 
         }
     };
--- app/sequences/s1.js
+++ app/sequences/s1.js
@@ -5,19 +5,29 @@ const i18n = require('i18n');
 const s1 = {
     id: 'sq1',
 
-    panels: [{
+    panels: [
+        {
             type: 'title',
             text: i18n.__('El hambre es una mujer africana hambrienta, con muchos hijos, que no va a la escuela y no tiene trabajo, nacida en un país seco, corrupto, estéril. Una imagen repetida infinitas veces que no nos deja ver el resto.')
-        }, {
-            type: 'vff',
-            height: 10000,
-            video: 's1p1faf.mp4',
-            audio: 's1p1faf.mp3'
-        }, {
-            type: 'autoplay',
-            video: 's1p2.mp4'
-        }, {
-            type: 'timeline'
+        },
+        // {
+        //     type: 'vff',
+        //     height: 10000,
+        //     video: 's1p1faf.mp4',
+        //     audio: 's1p1faf.mp3'
+        // },
+        // {
+        //     type: 'autoplay',
+        //     video: 's1p2.mp4'
+        // },
+        {
+            type: 'timeline',
+            init: 1968,
+            end: 2015
+        },
+        {
+            type: 'title',
+            text: i18n.__('Fin de secuencia')
         }
         /*, {
                 type: 'viei',
